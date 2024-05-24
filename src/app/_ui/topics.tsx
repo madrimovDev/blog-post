7ad6bf842a8bdf6710a5cd1593522a726e2e7588
madrimovDev <madrimov5014@gmail.com>
2024-05-24 18:59:57 +0500

    fix: :bug: fuxed

--- src/app/_ui/topics.tsx
+++ src/app/_ui/topics.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
 import { getBlogs, getTopics } from "../_utils/fetcher";
 import TopicItems from "./topic-items";
 
 export default async function Topics() {
 	const clearedTopics = await getTopics()
 	clearedTopics.unshift("all");
-	return <TopicItems topics={clearedTopics} />;
+	return <Suspense>
+		<TopicItems topics={clearedTopics} />
+	</Suspense>;
 }
